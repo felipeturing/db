5e97dafcb05a948dc1ef34a592f9fb49260eee79
felipeturing <wfelipet@uni.pe>
2022-05-26 21:34:24 -0500

    Purge

--- mariadb/insert.sql
+++
@@ -1,7 +0,0 @@
-USE test;
-
-INSERT INTO books (Title,SeriesID,AuthorID)
-VALUES('The Fellowship of the Ring',1,1),
-      ('The Two Towers',1,1), ('The Return of the King',1,1),
-      ('The Sum of All Men',2,2), ('Brotherhood of the Wolf',2,2),
-      ('Wizardborn',2,2), ('The Hobbbit',0,1);
